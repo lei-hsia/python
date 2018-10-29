176357cc06bebe23d7a31feed4eeb3584e77325c
LEI XIA <thomaslei1995@gmail.com>
2018-10-29 15:36:02 -0400

    Update 63_generator_factor.py

--- ds/63_generator_factor.py
+++ ds/63_generator_factor.py
@@ -10,3 +10,5 @@ def factor(n):
 	if k * k == n:  # special case if n is perfect square
 		yield k
 
+# https://letstalkdata.com/2015/05/how-to-use-python-generators-to-save-memory/
+# python中的generator类似于C++中的dynamic binding, 只会在需要的每一次动态生成，而不会从一开始就都生成好
