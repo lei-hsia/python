e699d4b44997dc3696c6e16e992c17f8a77295d9
LEI XIA <thomaslei1995@gmail.com>
2019-07-10 23:11:11 -0400

    Add files via upload

---
+++ IB_das_Praktitum/resize.py
@@ -0,0 +1,16 @@
+
+from PIL import Image
+from sys import argv
+
+if len(argv) != 4:
+	exit("Usage: python resize.py n infile outfile")
+
+n = int(argv[1])
+infile = argv[2]
+outfile = argv[3]
+
+inimage = Image.open(infile)
+width, height = inimage.size
+outimage = inimage.resize((width * n, height * n))
+
+outimage.save(outfile)
