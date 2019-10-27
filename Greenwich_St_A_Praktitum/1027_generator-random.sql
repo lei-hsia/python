9cf948de5df2211558553905942df26ddbfb7d60
LEI XIA <lx2196@columbia.edu>
2019-10-27 16:49:46 -0400

    Update 1027_generator-random.sql

--- Greenwich_St_A_Praktitum/1027_generator-random.sql
+++ Greenwich_St_A_Praktitum/1027_generator-random.sql
@@ -1,1 +1,5 @@
 select random(4711) from table(generator(rowcount => 5));
+
+-- generator: generators ROWCOUNT rows values;
+-- seq4(): index, starting from 0                                         
+select seq4(), uniform(1, 10, random(12)) from table(generator(rowcount => 100)) v;
